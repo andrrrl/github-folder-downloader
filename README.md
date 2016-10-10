d80313dcf5fdac50209fc8b93ca09147887dfc7d
Daniela <andresin@gmail.com>
2016-10-10 12:29:58 -0300

    added target folder option

--- README.md
+++ README.md
@@ -4,11 +4,9 @@
 
 - Downloads any project (sub)folder from github, to avoid downloading tests, demos, readme, etc, etc
 
-- Includes all the tree structure (and files) from the selected folder
+- Includes full tree structure (and files) from the selected folder as starting point
 
-- It defaults to php-maxcdn library (/MaxCDN/php-maxcdn)
-
-- Download happens in current local directory by default (TODO: allow target folder)
+- Download happens in current local directory by default
 
 ### Usage example: ###
 
@@ -16,18 +14,16 @@ Example project is https://github.com/MaxCDN/php-maxcdn/
 
 Let's say you want to download only the `/src` folder:
 
-1. Place this script inside the folder where to download and `cd` into it:     
-`$ cd my-local-folder/libraries/php-maxcdn/`
 
-2. Copy the folder path from the address bar, without the https://github.com part and without the trailing slash:    
+1. Copy the folder path from the address bar, without the https://github.com part and without the trailing slash:    
     - Given the URL: https://github.com/MaxCDN/php-maxcdn/tree/master/src/
     - Extract: `/MaxCDN/php-maxcdn/tree/master/src`
 
 
-3. Pass it to the script:    
-`$ python updatelib.py "/MaxCDN/php-maxcdn/tree/master/src"`
+2. Pass it to the script, along with the target folder:    
+`$ python updatelib.py "/MaxCDN/php-maxcdn/tree/master/src" /my/target/dir/`
 
 
 ### TODOs: ###
 
-[ ] Allow passing a target folder
+- [X] Allow passing a target dir/folder
--- gdownload.py
+++ gdownload.py
@@ -8,16 +8,13 @@ from lxml.html import parse
 from urllib import urlopen, URLopener, urlretrieve
 
 """
-    - ~ by Andrrr <andresin@gmail.com>
-    - Created: 20161007
-    - Revised: 20161009, 20161010
     - Downloads any project (sub)folder from github, to avoid downloading tests, demos, readme, etc, etc
     - Includes all the tree structure (and files) from the selected folder
-    - It defaults to php-maxcdn library (/MaxCDN/php-maxcdn)
-    - Download happens in current local directory by default (TODO: allow target folder)
+    - It defaults to php-maxcdn library (/MaxCDN/php-maxcdn) and target folder defaults to ./ (current dir)
+    - Download happens in current local directory by default 
     - Example, download only the /src folder of MaxCDN/php-maxcdn:
     $ cd my-project-folder/libraries/php-maxcdn/
-    $ python updatelib.py "/MaxCDN/php-maxcdn/tree/master/src"
+    $ python updatelib.py "/MaxCDN/php-maxcdn/tree/master/src" /my/target/dir/
     - Tip: copy the full github folder path from the address bar, without the https://github.com part
     
 """
@@ -26,14 +23,24 @@ class getSrcFromGit():
     github = "https://github.com"
     githubraw = "https://raw.githubusercontent.com"
     localfolder = ""
-    phpfile = ""
     
-    def __init__(self, gitlib):
+    def __init__(self, args):
         
-        if len(gitlib) > 1: 
-            self.library = gitlib[1] 
+        if len(args) == 1:
+            print "Github project required!"
+            print "Usage: $ python gdownload.py \"/githubuser/project/tree/master/some/folder\" /my/target/dir/"
+            exit()
+        
+        if len(args) > 1: 
+            self.library = args[1]
+        
+        if len(args) > 2:
+            self.target_dir = args[2]
         else: 
-            self.library = "/MaxCDN/php-maxcdn/tree/master/src"
+            self.target_dir = "./"
+
+        if not os.path.exists(self.target_dir):
+            os.makedirs(self.target_dir)
         
         self.url = self.github + self.library
         
@@ -42,7 +49,7 @@ class getSrcFromGit():
             print "Loading %s source from github... (%s)" % (matched.group().replace('/',''), self.url)
             parse(urlopen(self.url)).getroot()
         except IOError:
-            print "Invalid github URL, check"
+            print "Invalid github URL, please check"
             exit()
     
 
@@ -57,31 +64,34 @@ class getSrcFromGit():
 
         source = self.load_source()
 
+        # This comes from github.com site, it could change in the future
         body = source.cssselect(".css-truncate .js-navigation-open")
 
         file_list = []
         dir_list = []
 
         for f in body:
-        
-            php = f.text_content().split('.')
+            
+            current_item = f.text_content().split('.')
 
-            if len(php) > 1:
+            # Quick and dirty way to see if is a file or folder
+            if len(current_item) > 1:
                 file_list.append(f.text_content())
-                print 'Downloading %s to ./%s' % (f.text_content(), (self.localfolder + f.text_content()))
-                urlretrieve (self.githubraw + self.library.replace('/tree', '') + '/' + self.localfolder + f.text_content(), './' + self.localfolder + f.text_content())
+                print 'Downloading %s to %s' % (f.text_content(), (self.target_dir + self.localfolder + f.text_content()))
+                urlretrieve (self.githubraw + self.library.replace('/tree', '') + '/' + self.localfolder + f.text_content(), self.target_dir + self.localfolder + f.text_content())
             else:
                 dir_list.append(f.text_content())
 
         for item in sorted(dir_list):
 
             self.localfolder = self.localfolder + item + '/'
-            print "Creating subfolder: %s" % (self.localfolder)
+            print "Creating subfolder: %s" % (self.target_dir + self.localfolder)
 
-            if not os.path.exists(self.localfolder):
-                os.makedirs(self.localfolder)
+            if not os.path.exists(self.target_dir + self.localfolder):
+                os.makedirs(self.target_dir + self.localfolder)
 
             self.process_source(self.url + '/' + item)
+
 
 if __name__ == '__main__':
     getSrcFromGit(sys.argv).process_source()
